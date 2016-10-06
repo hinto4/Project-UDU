4becbe6dcbc65718e33b0bfd6a434b1959ed48ee
hinto4 <hinto.harri@gmail.com>
2016-10-07 00:17:38 +0300

    CalcMovementFaceDirection - Rotates the character towards moving direction.

--- Assets/_Scripts/Ball.cs
+++ Assets/_Scripts/Ball.cs
@@ -1,7 +1,9 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-// lisasin komentaari
+using Random = UnityEngine.Random;
+
 public class Ball : MonoBehaviour
 {
     public int MaxDamage = 100;
@@ -9,37 +11,40 @@ public class Ball : MonoBehaviour
     public int Damage;
     public GameObject DamageIndicatorPrefab;
 
-    // Mingi muudatus siin.
     private BonusManager _bonusManager;
+
+    private Vector3 _previousPosition;
 
     void Start()
     {
         _bonusManager = GameObject.FindObjectOfType<BonusManager>();
+        _previousPosition = transform.position;                     // Starting off last position as current position
     }
 
-    void Update()
+    void Update()   
     {
         DamageManager();
+        CalcMovementFaceDirection();
+    }
+
+    void CalcMovementFaceDirection()       // TODO add Quaternion.Slerp for smooth rotation for the character between axis + speed controller.
+    {
+        //Debug.Log(transform.InverseTransformDirection(this.gameObject.GetComponent<Rigidbody2D>().velocity));
+        Vector3 positionFor2D = new Vector3(this.transform.position.x, this.transform.position.y, 0f);
+
+        Vector3 deltaPosition = positionFor2D - _previousPosition;  // Calculate position change between frames
+
+        if (deltaPosition != Vector3.zero)
+        {
+            transform.up = deltaPosition;                           // rotates Y axis as facing axis towards the movement.
+        }
+
+        _previousPosition = transform.position;                     // Recording current position as previous position for next frame
     }
 
     void DamageManager()
     {
         Damage = Random.Range(MinDamage, MaxDamage);
-    }
-
-    void OnCollisionEnter2D(Collision2D col)
-    {
-        if (col.gameObject.GetComponent<Obstacle>())
-        {
-            ShowDamagePopUp();
-        }
-
-        if (col.gameObject.GetComponent<Bonus>())
-        {
-            _bonusManager.Bonus += 20;              // TODO will be changed, bonus value depends on the value and type of the picked up bonus.
-            _bonusManager.UpdateBonusValue();
-            _bonusManager.RemoveBonusItem(col.gameObject.GetComponent<Bonus>());
-        }
     }
 
     void ShowDamagePopUp()
@@ -62,5 +67,20 @@ public class Ball : MonoBehaviour
         }
 
         damageIndicator.GetComponent<TextMesh>().text = Damage.ToString();
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.GetComponent<Obstacle>())
+        {
+            ShowDamagePopUp();
+        }
+
+        if (col.gameObject.GetComponent<Bonus>())
+        {
+            _bonusManager.Bonus += 20;              // TODO will be changed, bonus value depends on the value and type of the picked up bonus.
+            _bonusManager.UpdateBonusValue();
+            _bonusManager.RemoveBonusItem(col.gameObject.GetComponent<Bonus>());
+        }
     }
 }
