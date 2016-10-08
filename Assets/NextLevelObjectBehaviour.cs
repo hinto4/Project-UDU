c9536be9bbf51297ca5909684ea0ec62d6e79944
hinto4 <hinto.harri@gmail.com>
2016-10-08 20:04:05 +0300

    NextLevel behaviour, early stage.

---
+++ Assets/NextLevelObjectBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(LevelManager))]
+public class NextLevelObjectBehaviour : MonoBehaviour
+{
+    private LevelManager _levelManager;
+
+	void Start ()
+    {
+        _levelManager = GameObject.FindObjectOfType<LevelManager>();    
+	}
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        GameObject obj = col.gameObject;
+        if(obj.tag == "Player")
+        {
+            _levelManager.GenerateNewLevel();
+
+            Destroy(this.gameObject);
+        }
+    }
+}
