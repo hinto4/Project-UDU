ad234727d57366069cf0ab7b18fada45ec7de772
hinto4 <hinto.harri@gmail.com>
2016-10-09 20:03:17 +0300

    Options Panel

--- Assets/_Scripts/MainMenu/MenuManager.cs
+++ Assets/_Scripts/MainMenu/MenuManager.cs
@@ -4,8 +4,15 @@ using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
+    public GameObject OptionsPanel;
+
     public void StartGame(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
+    }
+
+    public void Options()
+    {
+        OptionsPanel.SetActive(true);
     }
 }
