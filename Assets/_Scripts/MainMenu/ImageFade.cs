ead90e652d61ce137ca1e7b39cb5ccc2699a4859
hinto4 <hinto.harri@gmail.com>
2016-10-09 19:59:29 +0300

    Main menu - first Stage.

---
+++ Assets/_Scripts/MainMenu/ImageFade.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ImageFade : MonoBehaviour
+{
+    public int FadeTime;
+
+    private Image _image;
+    private float _startTime;
+
+    void Start()
+    {
+        _image = this.GetComponent<Image>();
+        _startTime = Time.time + FadeTime;
+    }
+
+    void Update()
+    {
+        if (_startTime - Time.time  <= 0)
+        {
+            _image.color = Color.Lerp(_image.color, new Color(0, 0, 0, 0.75f), Time.deltaTime * 2);
+            Invoke("DestroyFadeObject", FadeTime);
+        }
+    }
+
+    void DestroyFadeObject()
+    {
+        Destroy(this.gameObject);
+    }
+}
---
+++ Assets/_Scripts/MainMenu/MenuManager.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public void StartGame(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
