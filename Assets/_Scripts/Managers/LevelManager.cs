2b724cbefe7f06087fd2d99f0a09dddc9829d680
hinto4 <hinto.harri@gmail.com>
2016-10-11 17:27:22 +0300

    Work on level generator. Detect close objects, if there's something close, generate new vector 2 position.

--- Assets/_Scripts/Managers/LevelManager.cs
+++ Assets/_Scripts/Managers/LevelManager.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class LevelManager : MonoBehaviour
 {
@@ -29,32 +30,59 @@ public class LevelManager : MonoBehaviour
 
     public void GenerateNewLevel()
     {
-        //TEMP CODE For generation debugger
+        float minPos = -2;          // Limit the spawn space. Will be replaced with prefab.
+        float maxPos = 2;
+        float detectDistance = 2.4f;  // The distance between the closest rock.
 
-        foreach (var obs in FindObjectsOfType<Obstacle>())
+        foreach (var obs in FindObjectsOfType<Obstacle>())      // Finds if there's any left over obstacles, destroys them if so.
         {
             if (obs == null)
                 return;
             Destroy(obs.gameObject);
         }
 
-        float minPos = -2;
-        float maxPos = 2;
+        List<Vector2> positionBuffer = new List<Vector2>();     // For storing the set positions.
 
         for (int i = 1; i <= _spawnObstaclesCount; i++)
         {
-            Quaternion randomRotation = Quaternion.Euler(0, 0, Random.Range(0, 360));
             Vector2 newRandomPosition = new Vector2(Random.Range(minPos, maxPos), Random.Range(minPos, maxPos));
-            int randomObstacle = Random.Range(0, Obstacles.Length);
+            positionBuffer.Add(newRandomPosition);
 
-            GameObject obstaclePrefab = Instantiate(Obstacles[randomObstacle], this.transform.position, randomRotation) as GameObject;
+        }
+        // Still broken......
+        for (int i = 0; i < positionBuffer.Count; i++)
+        {
+            if (positionBuffer[i] != positionBuffer[positionBuffer.Count - 1])      // if the loop has reached to the end of the List size.
+            {
+                Vector2 offset = positionBuffer[i] - positionBuffer[i + 1];
+                float sqrLen = offset.sqrMagnitude;
+
+                if (sqrLen < Mathf.Pow(detectDistance, 2))
+                {
+                    positionBuffer.Remove(positionBuffer[i]);
+
+                    Vector2 newRandomPosition = new Vector2(Random.Range(minPos, maxPos), Random.Range(minPos, maxPos));
+                    positionBuffer.Add(newRandomPosition);
+                }
+            }
+        }
+
+        foreach (var obstaclePosition in positionBuffer)
+        {
+            int randomObstacle = Random.Range(0, Obstacles.Length);     // Choose random obstacle from the Obstacles array.
+            Quaternion randomRotation = Quaternion.Euler(0, 0, Random.Range(0, 360));
+
+            GameObject obstaclePrefab = Instantiate(Obstacles[randomObstacle],
+                this.transform.position, randomRotation) as GameObject;
 
             if (obstaclePrefab == null)
                 return;
 
             obstaclePrefab.transform.SetParent(GameObject.FindGameObjectWithTag("WorldObjects").transform);
-            obstaclePrefab.transform.position = newRandomPosition;
+            obstaclePrefab.transform.position = obstaclePosition;
         }
+
+        positionBuffer.Clear(); // Clear the buffer.
     }
 
     public void SetObstaclesSpawnCount(int spawnCount)
